35570e32f537209ed6e61c6d7845b524edb175c8
rodolfoSantosUK <santoseuro2018@gmail.com>
2021-02-01 20:07:33 -0300

    Producer and Consumer em tempo real.

--- rabbitmq-producer/src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
+++ rabbitmq-producer/src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RabbitmqProducerApplication implements CommandLineRunner {
 
-	@Autowired
-	private HelloRabbitProducer helloRabbitProducer;
+//	@Autowired
+//	private HelloRabbitProducer helloRabbitProducer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqProducerApplication.class, args);
@@ -18,6 +20,6 @@ public class RabbitmqProducerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		helloRabbitProducer.sendHello("Rodolfo" + Math.random());
+	//	helloRabbitProducer.sendHello("Rodolfo" + Math.random());
 	}
 }
