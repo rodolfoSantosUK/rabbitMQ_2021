529a9f00f385671ab8fa6928da2dbb2e1c1daf39
rodolfoSantosUK <santoseuro2018@gmail.com>
2021-02-03 20:54:18 -0300

    Projeto numero 2 do curso de rabbitMq

---
+++ rabbitmq-consumer-two/src/main/java/com/course/rabbitmqconsumertwo/RabbitmqConsumerTwoApplication.java
@@ -0,0 +1,13 @@
+package com.course.rabbitmqconsumertwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqConsumerTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqConsumerTwoApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-consumer-two/src/test/java/com/course/rabbitmqconsumertwo/RabbitmqConsumerTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.rabbitmqconsumertwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqConsumerTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
